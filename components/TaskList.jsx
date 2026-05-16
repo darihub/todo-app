65fa5616a046864a6d3bc5ca8fa0241157c951d2
darihub <gramarg85@gmail.com>
2026-05-15 23:04:22 -0300

    change: changed TaskList.jsx header

--- components/TaskList.jsx
+++ components/TaskList.jsx
@@ -3,7 +3,7 @@ import TaskItem from './TaskItem';
 export default function TaskList({tasks = []}) {
   return (
     <div>
-      <h1>My To-Do List</h1>
+      <h1>Tasks:</h1>
       {tasks.map((task, index) => (
         <TaskItem key={task.id} task={task} />
       ))}
