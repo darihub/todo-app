e9b1dd3124aa0caf22450004feffa43cdae5e3ee
darihub <gramarg85@gmail.com>
2026-05-15 23:05:23 -0300

    feat: implemented page.jsx Home() functionality and addTask()

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,27 @@
+"use client";
+
 import TaskItem from '../components/TaskItem';
 import TaskList from '../components/TaskList';
+import TaskInput from '../components/TaskInput';
+import { useState } from 'react';
 import './globals.css';
 
+function addTask(text) {
+  const newTask = {
+    id: Date.now(),
+    text,
+    completed: false
+  };
+  setTasks([...tasks, newTask]);
+  }
+
 export default function Home() {
+  const [tasks, setTasks] = useState([]);
   return (
     <main>
-      <TaskList task1={{ id: 1, text: 'Buy groceries', completed: false }} task2={{ id: 2, text: 'Walk the dog', completed: true }} />
+    <h1>My To-Do List</h1>
+    <TaskInput onAdd={addTask} />
+    <TaskList tasks={tasks} />
     </main>
   );
 }
