45fe117114e8063ec740df8fbc6407a498215a50
darihub <gramarg85@gmail.com>
2026-05-15 22:26:12 -0300

    feat: implementa componente TaskInput.jsx

--- components/TaskInput.jsx
+++ components/TaskInput.jsx
@@ -1,12 +1,18 @@
-import TaskItem from './TaskItem';
-import TaskList from './TaskList';
+import { useState } from 'react';
 
-export default function TaskInput(onAdd) {
+export default function TaskInput( {onAdd }) {
+  const [text, setText] = useState('');
+
   return (
     <div>
       <h2>Add a new task</h2>
-      <input type="text" placeholder="Enter task title" />
-      <button onClick={() => onAdd(type)}>Add Task</button>
+      <input type="text" 
+        placeholder="Enter task title" 
+        value={text}
+        onChange={(e) => setText(e.target.value)} 
+        /* e: evento, e.target: input, e.target.value: valor del input (texto actual escrito) */
+      />
+      <button onClick={() => onAdd(text)}>Add Task</button>
     </div>
   );
 }
